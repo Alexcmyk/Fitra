df21928d18d06bcb60fff34b22c86bf821713aaa
Alexcmyk <alex.kirts@gmail.com>
2021-03-18 22:02:18 -0600

    removed jquery, add JS

--- assets/application.js
+++ assets/application.js
@@ -1,8 +1,14 @@
-$(document).ready(function () {
-  $(document).on('change', '#variant-id', function () {
-    let variant_id = this.value;
-    let variant_image = $('.single-product-image[data-variant="' + variant_id + '"]');
-
-    variant_image.show().siblings(':visible').hide();
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  let option = document.getElementById('variant-id');
+  if (option) {
+    option.addEventListener('change', function (e) {
+      let target = e.target.value;
+      let productImage = document.querySelector(`.single-product-image[data-variant="${target}"]`);
+      let productImages = document.querySelectorAll('.single-product-image');
+      productImages.forEach((image) => {
+        image.style.display = 'none';
+      });
+      productImage.style.display = 'block';
+    });
+  }
 });
